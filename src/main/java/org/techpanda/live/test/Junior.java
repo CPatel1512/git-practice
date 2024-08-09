6862cb32a0519f913ceff27b0712e401ad8ece55
Chetan <chetanpatel151281@gmail.com>
2024-08-09 10:30:43 +0100

    git commit -m 'done'

---
+++ src/main/java/org/techpanda/live/test/Junior.java
@@ -0,0 +1,7 @@
+package org.techpanda.live.test;
+
+public class Junior {
+    public static void main(String[] args) {
+        System.out.println("i am in junior");
+    }
+}
---
+++ src/main/java/org/techpanda/live/test/Senior.java
@@ -0,0 +1,9 @@
+package org.techpanda.live.test;
+
+public class Senior {
+    public static void main(String[] args) {
+        System.out.println("I am In Senior class");
+    }
+
+
+}
